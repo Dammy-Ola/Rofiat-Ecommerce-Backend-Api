ea851881d41e76bbb68e084ea6f356556f9c0d9b
Dammy Ola <dammyolapaix@gmail.com>
2022-12-08 00:31:37 +0000

    Connect the app to a mongodb database

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import connectDB from './api/v1/config/db'
 
 dotenv.config()
 
@@ -22,13 +23,31 @@ const app = express()
 /**
  *  App Configuration
  */
+// Enabling cors
+const corsOptions = {
+  origin: process.env.FRONTEND_URL as string,
+  credentials: true as boolean,
+  optionsSuccessStatus: 200 as number, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(helmet())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 /**
  * Server Activation
  */
-app.listen(PORT, () => {
-  console.log(`App Listening on port ${PORT}`)
-})
+const start = async () => {
+  try {
+    await connectDB()
+    console.log(`MongoDB connected`)
+
+    app.listen(PORT, () => {
+      console.log(`App Listening on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
