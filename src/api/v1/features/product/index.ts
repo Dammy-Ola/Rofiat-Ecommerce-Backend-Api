fe798996a9ccb206cb91ceb44fac7e19da594112
Dammy Ola <dammyolapaix@gmail.com>
2022-12-08 17:29:59 +0000

    Imported the Product model

--- src/api/v1/features/product/index.ts
+++ src/api/v1/features/product/index.ts
@@ -1,3 +1,4 @@
+import Product from './Product'
 import productRoutes from './product.routes'
 
-export default productRoutes
+export { Product, productRoutes }
--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-import productRoutes from '../features/product'
+import { productRoutes } from '../features/product'
 
 router.use('/api/v1/products', productRoutes)
 
